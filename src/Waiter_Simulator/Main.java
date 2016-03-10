71965a8f8bbcd8b1d6aa5faf5c86209360287d52
Luke <4222@pierson.nl>
2016-03-10 09:59:01 +0100

    Small fix
    Fixing a small error

--- src/Waiter_Simulator/Main.java
+++ src/Waiter_Simulator/Main.java
@@ -132,7 +132,7 @@ public class Main extends Canvas implements Runnable{
 
 		for(int x = 0; x < map.length; x++) {
 			for(int y = 0; y < map[0].length; y++) {
-				if(map[x][y].intersects(p) && map[x][y].getID() == 1) {
+				if(map[x][y].intersects(p) && map[x][y].isSolid()) {
 					System.out.println(x + " " + y);
 				}
 			}
