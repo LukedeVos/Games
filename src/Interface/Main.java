64297739941f8b91f131e9b1a54be800c77c5fa1
Milisar <4240@pierson.nl>
2016-09-03 16:55:32 +0200

    Yay ik doe iets
    Heb wel helemaal 2 plaatjes toegevoegt. Nu kan je de profiles en
    settings tab in en uit.

--- src/Interface/Main.java
+++ src/Interface/Main.java
@@ -17,6 +17,9 @@ public class Main extends Canvas implements Runnable {
 	public static final int SCALE = 2;
 	public final String TITLE = "Interface";
 	
+	private int inMenu = 1;
+	private int settings = 0;
+	private int profile = 0;
 	private int select = 2;
 	private boolean unpressed = true;
 	private static Main game = new Main();
@@ -24,7 +27,7 @@ public class Main extends Canvas implements Runnable {
 	
 	private boolean running = false;
 	private Thread thread;
-	private BufferedImage inter, loaded, s0, s1, s2, s3, s4;
+	private BufferedImage inter, loaded, s0, s1, s2, s3, s4, s5, s6;
 
 
 	public void init(){
@@ -38,6 +41,8 @@ public class Main extends Canvas implements Runnable {
 			s2 = loader.loadImage("/res/Interface/Interface_2.png");
 			s3 = loader.loadImage("/res/Interface/Interface_3.png");
 			s4 = loader.loadImage("/res/Interface/Interface_4.png");
+			s5 = loader.loadImage("/res/Interface/Interface_5.png");
+			s6 = loader.loadImage("/res/Interface/Interface_6.png");
 		} catch(IOException e){
 			e.printStackTrace();
 		}
@@ -103,19 +108,23 @@ public class Main extends Canvas implements Runnable {
 		//////////////////////////////////
 		
 		g.drawImage(inter, 0, 0, null);
-		
-		if(select == 0){
-			g.drawImage(s0, 0, 0, null);
-		} else if(select == 1){
-			g.drawImage(s1, 0, 0, null);
-		} else if(select == 2){
-			g.drawImage(s2, 0, 0, null);
-		} else if(select == 3){
-			g.drawImage(s3, 0, 0, null);
-		} else if(select == 4){
-			g.drawImage(s4, 0, 0, null);
+		if(inMenu == 1 && profile == 0 && settings == 0){
+			if(select == 0){
+				g.drawImage(s0, 0, 0, null);
+			} else if(select == 1){
+				g.drawImage(s1, 0, 0, null);
+			} else if(select == 2){
+				g.drawImage(s2, 0, 0, null);
+			} else if(select == 3){
+				g.drawImage(s3, 0, 0, null);
+			} else if(select == 4){
+				g.drawImage(s4, 0, 0, null);
+			}
+		}else if(inMenu == 0 && profile == 1 && settings == 0){
+			g.drawImage(s5, 0, 0, null);
+		}else if(inMenu == 0 && profile == 0 && settings == 1){
+			g.drawImage(s6, 0, 0, null);
 		}
-		
 		//////////////////////////////////
 		g.dispose();
 		bs.show();
@@ -123,6 +132,16 @@ public class Main extends Canvas implements Runnable {
 
 	public void keyPressed(KeyEvent e){
 		int key = e.getKeyCode();
+		//////////////////////////////////
+		if(profile == 1 || settings == 1){
+			if(key == KeyEvent.VK_ESCAPE){
+				profile = 0;
+				settings = 0;
+				inMenu = 1;
+			}
+		//simpel nu ff failsafe ofc ga ik verbeteren//
+		//////////////////////////////////	
+		}
 		
 		if(unpressed){
 			if(key == KeyEvent.VK_W){
@@ -162,6 +181,9 @@ public class Main extends Canvas implements Runnable {
 		
 		if(key == KeyEvent.VK_ENTER){
 			if(select == 0){
+				inMenu = 0;
+				profile = 1;
+				settings = 0;
 				
 			} else if(select == 1){
 			
@@ -172,7 +194,9 @@ public class Main extends Canvas implements Runnable {
 				System.exit(0);
 				frame.dispose();
 			} else if(select == 4){
-				
+				inMenu = 0;
+				profile = 0;
+				settings = 1;
 			}
 		}
 		
